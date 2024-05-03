ade4116427aa050c2f177ea239951a536f1a3b1a
fernando-carrion <fcarrion@apple.com>
2024-05-03 14:25:37 -0300

    Override the Security configuration

--- auth-api/src/main/java/com/auth/jwt/authapi/services/JwtService.java
+++ auth-api/src/main/java/com/auth/jwt/authapi/services/JwtService.java
@@ -43,7 +43,7 @@ public class JwtService {
     }
 
     public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
-        return buildToken(extraClaims, userDetails, jwtExpiration);
+        return buildToken(extraClaims, userDetails, this.jwtExpiration);
     }
 
     private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long expiration) {
